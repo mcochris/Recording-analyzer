5d4cc5e4c053144a5e7120876d5956acf0f1b89f
Chris Strawser <mcochris@users.noreply.github.com>
2026-04-02 19:03:08 -0400

    Add check.sh: Implement loudness range analysis script for audio files

---
+++ verification/loudness range/check.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+#
+# This script runs the loudness range analysis scripts on a given audio file
+# This script is called by verify.sh
+#
+
+#set -o xtrace
+
+[[ $# -eq 0 ]] && { echo "Normally, this script is called by verify.sh
+Usage: $0 --loudness-range <value> <audio_file>
+Optional: --debug"; exit 1; }
+
+# shellcheck disable=SC1091
+source ../common.sh
+
+AUDIO_FILE=""
+DEBUG=""
+LOUDNESS_RANGE=""
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -d|--debug)
+            DEBUG="$1"
+            shift
+            ;;
+        --loudness-range)
+            LOUDNESS_RANGE="$2"
+            shift 2
+            ;;
+        *)
+			AUDIO_FILE="$1"
+			shift
+			;;
+    esac
+done
+
+debug "Starting loudness range check for $AUDIO_FILE"
+debug "Loudness range: $LOUDNESS_RANGE"
+
+readonly LOUDNESS_RANGE AUDIO_FILE DEBUG
+
+check_audio_file "$AUDIO_FILE"
+
+[[ -z "$LOUDNESS_RANGE" ]] && { echo "$0: Error: No loudness range specified"; exit 1; }
+
+is_number "$LOUDNESS_RANGE" || { echo "$0: Error: Loudness range is not a valid number"; exit 1; }
+
+./loudness_range.sh "$DEBUG" --loudness-range "$LOUDNESS_RANGE" "$AUDIO_FILE"
