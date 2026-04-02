cc7018b5f7fb84a929b24dc938bcb63fb9ff81a2
Chris Strawser <mcochris@users.noreply.github.com>
2026-04-02 03:38:47 -0400

    Refactor check.sh: Improve error handling and simplify audio file argument processing

--- verification/noise floor/check.sh
+++ verification/noise floor/check.sh
@@ -6,11 +6,6 @@
 #
 
 #set -o xtrace
-set -o errexit
-set -o nounset
-set -o pipefail
-set -o errtrace
-trap 'echo "ERROR: line $LINENO command \"$BASH_COMMAND\" exited with status $?" >&2' ERR
 
 [[ $# -eq 0 ]] && { echo "Normally, this script is called by verify.sh
 Usage: $0 --left-noise-floor <value> --right-noise-floor <value> <audio_file>
@@ -39,38 +34,33 @@ while [[ $# -gt 0 ]]; do
             RIGHT_NOISE_FLOOR="$2"
             shift 2
             ;;
-        --audio-file)
-			AUDIO_FILE="$2"
-			shift 2
-			;;
 		*)
+			AUDIO_FILE="$1"
 			shift
 			;;
     esac
 done
 
 debug "Starting noise floor check for $AUDIO_FILE"
+debug "Left noise floor: $LEFT_NOISE_FLOOR"
+debug "Right noise floor: $RIGHT_NOISE_FLOOR"
 
-[[ -z "$AUDIO_FILE" ]] && { echo "$0: Error: No audio file specified"; exit 1; }
-[[ -e "$AUDIO_FILE" ]] || { echo "$0: Error: Audio file does not exist: $AUDIO_FILE"; exit 1; }
-[[ -f "$AUDIO_FILE" ]] || { echo "$0: Error: Audio file is not a regular file: $AUDIO_FILE"; exit 1; }
-[[ -r "$AUDIO_FILE" ]] || { echo "$0: Error: Audio file is not readable: $AUDIO_FILE"; exit 1; }
-debug "Audio file: $AUDIO_FILE"
+readonly AUDIO_FILE DEBUG LEFT_NOISE_FLOOR RIGHT_NOISE_FLOOR
+
+check_audio_file "$AUDIO_FILE"
 
 [[ -z "$LEFT_NOISE_FLOOR" && -z "$RIGHT_NOISE_FLOOR" ]] && { echo "$0: Error: No noise floor levels specified"; exit 1; }
 
 if [[ -n "$LEFT_NOISE_FLOOR" ]]; then
-    debug "Left noise floor: $LEFT_NOISE_FLOOR"
 	is_number "$LEFT_NOISE_FLOOR" || { echo "$0: Error: Left noise floor is not a valid number"; exit 1; }
 else
     debug "Left noise floor not specified"
 fi
 
 if [[ -n "$RIGHT_NOISE_FLOOR" ]]; then
-    debug "Right noise floor: $RIGHT_NOISE_FLOOR"
 	is_number "$RIGHT_NOISE_FLOOR" || { echo "$0: Error: Right noise floor is not a valid number"; exit 1; }
 else
     debug "Right noise floor not specified"
 fi
 
-python3 ./noise_floor.py "$AUDIO_FILE" "$LEFT_NOISE_FLOOR" "$RIGHT_NOISE_FLOOR" "$(get_threshold)" "$DEBUG"
+python3 ./noise_floor.py "$DEBUG" --left-noise-floor "$LEFT_NOISE_FLOOR" --right-noise-floor "$RIGHT_NOISE_FLOOR" --threshold "$(get_threshold)" "$AUDIO_FILE"
