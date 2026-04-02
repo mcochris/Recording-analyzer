8ca18031293f9313225fb1ef220866da98ce19bd
Chris Strawser <mcochris@users.noreply.github.com>
2026-04-02 03:39:28 -0400

    Refactor peak_level.sh: Improve error handling and simplify audio file checks

--- verification/peak level/peak_level.sh
+++ verification/peak level/peak_level.sh
@@ -6,11 +6,6 @@
 #
 
 #set -o xtrace
-set -o errexit
-set -o nounset
-set -o pipefail
-set -o errtrace
-trap 'echo "ERROR: line $LINENO command \"$BASH_COMMAND\" exited with status $?" >&2' ERR
 
 [[ $# -eq 0 ]] && { echo "Normally, this script is called by check.sh
 Usage: $0 --left-peak-level <value> --right-peak-level <value> <audio_file>
@@ -52,17 +47,14 @@ debug "Left peak level: $LEFT_PEAK_LEVEL"
 debug "Right peak level: $RIGHT_PEAK_LEVEL"
 debug "Threshold: ${THRESHOLD}%"
 
-[[ -z "$AUDIO_FILE" ]] && { echo "$0: Error: No audio file specified"; exit 1; }
-[[ -e "$AUDIO_FILE" ]] || { echo "$0: Error: Audio file does not exist: $AUDIO_FILE"; exit 1; }
-[[ -f "$AUDIO_FILE" ]] || { echo "$0: Error: Audio file is not a regular file: $AUDIO_FILE"; exit 1; }
-[[ -r "$AUDIO_FILE" ]] || { echo "$0: Error: Audio file is not readable: $AUDIO_FILE"; exit 1; }
+readonly AUDIO_FILE LEFT_PEAK_LEVEL RIGHT_PEAK_LEVEL THRESHOLD
+
+check_audio_file "$AUDIO_FILE"
 
 [[ -z "$LEFT_PEAK_LEVEL" && -z "$RIGHT_PEAK_LEVEL" ]] && { echo "$0: Error: No peak levels specified"; exit 1; }
 
-is_number "$LEFT_PEAK_LEVEL" || { echo "$0: Error: Left peak level is not a valid number"; exit 1; }
-is_number "$RIGHT_PEAK_LEVEL" || { echo "$0: Error: Right peak level is not a valid number"; exit 1; }
-
 if [[ -n "$LEFT_PEAK_LEVEL" ]]; then
+	is_number "$LEFT_PEAK_LEVEL" || { echo "$0: Error: Left peak level is not a valid number"; exit 1; }
 	debug "Checking left peak level for $AUDIO_FILE"
 	read -r max_amplitude < <(sox "$AUDIO_FILE" -n remix 1 stat 2>&1 |
 		grep --ignore-case "Maximum amplitude" |
@@ -83,6 +75,7 @@ if [[ -n "$LEFT_PEAK_LEVEL" ]]; then
 fi
 
 if [[ -n "$RIGHT_PEAK_LEVEL" ]]; then
+	is_number "$RIGHT_PEAK_LEVEL" || { echo "$0: Error: Right peak level is not a valid number"; exit 1; }
 	debug "Checking right peak level for $AUDIO_FILE"
 	read -r max_amplitude < <(sox "$AUDIO_FILE" -n remix 2 stat 2>&1|
 		grep --ignore-case "Maximum amplitude" |
