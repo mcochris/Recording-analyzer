753446e57912e61b84f8e73e2a0b9e56ce34789f
Chris Strawser <mcochris@users.noreply.github.com>
2026-04-02 03:39:08 -0400

    Refactor check.sh: Improve argument handling and error checking for audio file and peak levels

--- verification/peak level/check.sh
+++ verification/peak level/check.sh
@@ -6,11 +6,6 @@
 #
 
 #set -o xtrace
-set -o errexit
-set -o nounset
-set -o pipefail
-set -o errtrace
-trap 'echo "ERROR: line $LINENO command \"$BASH_COMMAND\" exited with status $?" >&2' ERR
 
 [[ $# -eq 0 ]] && { echo "Normally, this script is called by verify.sh
 Usage: $0 --left-peak-level <value> --right-peak-level <value> <audio_file>
@@ -38,23 +33,20 @@ while [[ $# -gt 0 ]]; do
             RIGHT_PEAK_LEVEL="$2"
             shift 2
             ;;
-        --audio-file)
-			AUDIO_FILE="$2"
-			shift 2
-			;;
-		*)
+        *)
+			AUDIO_FILE="$1"
 			shift
 			;;
     esac
 done
 
 debug "Starting peak level check for $AUDIO_FILE"
+debug "Left peak level: $LEFT_PEAK_LEVEL"
+debug "Right peak level: $RIGHT_PEAK_LEVEL"
 
-[[ -z "$AUDIO_FILE" ]] && { echo "$0: Error: No audio file specified"; exit 1; }
-[[ -e "$AUDIO_FILE" ]] || { echo "$0: Error: Audio file does not exist: $AUDIO_FILE"; exit 1; }
-[[ -f "$AUDIO_FILE" ]] || { echo "$0: Error: Audio file is not a regular file: $AUDIO_FILE"; exit 1; }
-[[ -r "$AUDIO_FILE" ]] || { echo "$0: Error: Audio file is not readable: $AUDIO_FILE"; exit 1; }
-debug "Audio file: $AUDIO_FILE"
+readonly AUDIO_FILE DEBUG LEFT_PEAK_LEVEL RIGHT_PEAK_LEVEL
+
+check_audio_file "$AUDIO_FILE"
 
 [[ -z "$LEFT_PEAK_LEVEL" && -z "$RIGHT_PEAK_LEVEL" ]] && { echo "$0: Error: No peak levels specified"; exit 1; }
 
@@ -72,6 +64,6 @@ else
     debug "Right peak level not specified"
 fi
 
-./peak_level.sh "$DEBUG" "$AUDIO_FILE" --left-peak-level "$LEFT_PEAK_LEVEL" --right-peak-level "$RIGHT_PEAK_LEVEL"
+./peak_level.sh "$DEBUG" --left-peak-level "$LEFT_PEAK_LEVEL" --right-peak-level "$RIGHT_PEAK_LEVEL" "$AUDIO_FILE"
 
-python3 ./peak_level.py "$AUDIO_FILE" "$LEFT_PEAK_LEVEL" "$RIGHT_PEAK_LEVEL" "$(get_threshold)" "$DEBUG"
+python3 ./peak_level.py "$DEBUG" --left-peak-level "$LEFT_PEAK_LEVEL" --right-peak-level "$RIGHT_PEAK_LEVEL" --threshold "$(get_threshold)" "$AUDIO_FILE"
