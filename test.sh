3c8f23dcacd9fd2b06fbfc537423f2914b781fc1
Chris Strawser <mcochris@users.noreply.github.com>
2026-04-03 19:42:00 -0400

    line ending test

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# This script calls all the check.sh scripts in all the verification subdirectories
+# This script is manually run by the user.
+
+line one
+
+
+line two
+line three
